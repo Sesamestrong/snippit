8384f49d030c852c3ee6b8124bfb4aade3a7eafc
Sesamestrong <doolie.healey@gmail.com>
2019-10-07 17:53:18 +0000

    Reset to no nano-cra

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { Landing } from "./Landing";
+import { NotFound } from "./404";
+import { Navbar } from "./Navbar";
+import { Profile } from "./Profile";
+import { Create } from "./Create";
+import { Docs } from "./Docs";
+import { Explore } from "./Explore";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const theme = {
+	dark: {
+		textColor: "#0c0",
+		textColorBright: "#0c0"
+	}
+};
 
-export default App;
+const indentation = [1, 1, 2, 3, 4, 3, 3, 2, 2];
+export default () => (
+	<Router>
+		<ThemeProvider theme={theme.dark}>
+			<>
+				<Navbar></Navbar>
+				<Switch>
+					<Route exact path="/" component={Landing}></Route>
+					<Route exact path="/docs" component={Docs}></Route>
+					<Route exact path="/explore" component={Explore}></Route>
+					<Route exact path="/profile" component={Profile}></Route>
+					<Route exact path="/new" component={Create}></Route>
+					<Route component={NotFound}></Route>
+				</Switch>
+			</>
+		</ThemeProvider>
+	</Router>
+);
